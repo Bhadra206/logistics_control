3e145c3bca76f523edc69cbe3a533124ffb39c9a
Bhadra R <bhadra.r@zeronetraining.local>
2025-09-24 15:25:11 +0530

    committed

--- backend/routes/Order.js
+++ backend/routes/Order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const orderController = require("../controllers/orderControllers");
 
 router.get("/getOrder", orderController.getOrders);
-router.get("/getAllOrder", orderController.getAllOrders);
+router.get("/getAllOrders", orderController.getAllOrders);
 router.get("/getOrderByIdOrName", orderController.getOrderByIdOrName);
 router.post("/createOrder", orderController.createOrder);
 router.put("/replaceOrder/:id", orderController.replaceOrder);
